9e16f10aeced3dd6941b348d2dcfe8c87da9a545
Eric de Jesus <58758891+ericejs@users.noreply.github.com>
2023-02-13 10:00:57 -0300

    Add files via upload

---
+++ pizza store + javascript + API/pizzas.js
@@ -0,0 +1,11 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:45.50, sizes:['6 Fatias', '8 Fatias', '10 Fatias'], description:'Descrição com o sabor e ingredientes usados na pizza...'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:45.50, sizes:['6 Fatias', '8 Fatias', '10 Fatias'], description:'Descrição com o sabor e ingredientes usados na pizza...'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:45.50, sizes:['6 Fatias', '8 Fatias', '10 Fatias'], description:'Descrição com o sabor e ingredientes usados na pizza...'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:45.50, sizes:['6 Fatias', '8 Fatias', '10 Fatias'], description:'Descrição com o sabor e ingredientes usados na pizza...'},
+    {id:5, name:'Manjericão', img:'images/pizza5.png', price:45.50, sizes:['6 Fatias', '8 Fatias', '10 Fatias'], description:'Descrição com o sabor e ingredientes usados na pizza...'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:45.50, sizes:['6 Fatias', '8 Fatias', '10 Fatias'], description:'Descrição com o sabor e ingredientes usados na pizza...'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:45.50, sizes:['6 Fatias', '8 Fatias', '10 Fatias'], description:'Descrição com o sabor e ingredientes usados na pizza...'},
+    {id:8, name:'Camarão', img:'images/pizza8.png', price:45.50, sizes:['6 Fatias', '8 Fatias', '10 Fatias'], description:'Descrição com o sabor e ingredientes usados na pizza...'},
+    {id:9, name:'Italiana', img:'images/pizza9.png', price:45.50, sizes:['6 Fatias', '8 Fatias', '10 Fatias'], description:'Descrição com o sabor e ingredientes usados na pizza...'}
+];
